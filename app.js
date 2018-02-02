950c85a627278d33e6edfc5b3987989468cdb226
JohanQuiroga <quirogacj@utp.edu.co>
2018-02-02 13:48:35 -0500

    Added update task functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var vm = new Vue({
 	el: '#app',
 	data: {
+		draft: '',
 		new_task: '',
 		tasks: [
 			{
@@ -11,7 +12,7 @@ var vm = new Vue({
 			{
 				description: 'Suscribirse a Styde.net',
 				pending: true,
-				editing: true
+				editing: false
 			},
 			{
 				description: 'Crear una API',
@@ -31,6 +32,28 @@ var vm = new Vue({
 
 				this.new_task = '';
 			}
+		},
+		toggleStatus: function (task) {
+			task.pending = !task.pending;
+		},
+		editTask: function (task) {
+			this.tasks.forEach(function (task) {
+				task.editing = false;
+			});
+
+			this.draft = task.description;
+
+			task.editing = true;
+		},
+		updateTask: function (task) {
+			if (this.draft != '') {
+				task.description = this.draft;
+			}
+
+			task.editing = false;
+		},
+		discardTask: function (task) {
+			task.editing = false;
 		}
 	}
 });
