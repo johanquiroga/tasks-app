afbbde6525049210dcf68b743dc928586965e201
Johan Quiroga <quirogacj@utp.edu.co>
2018-02-07 11:12:31 -0500

    Added 404 errors handling

---
+++ src/components/errors/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="text-center">
+		<img class="img" src="../../assets/404.jpg" alt="Error 404: página no encontrada">		
+
+		<h2>Error 404: página no encontrada</h2>
+
+		<router-link to="/">Regresar al dashboard</router-link>
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import App from './components/App.vue'
 
 window.EventBus = EventBus
 
+window.not_found = function () {
+	console.log('Not found: ' + router.currentRoute.fullPath);
+	
+	router.replace('/404');
+};
+
+window.not_found_unless = function (condition) {
+	if (!condition) {
+		not_found();
+	}
+};
+
 new Vue({
 	el: '#app',
 	router,
